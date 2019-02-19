bf9e5849423feef657e0b66729e7e830152dcb76
yahya <yahyaislamov15@mail.ru>
2019-02-19 22:01:02 +0300

    добавил новый проект

---
+++ lab1/ds-1-master/src/Frontend/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Frontend.Models;
+using System.Net.Http;
+
+namespace Frontend.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<string> UploadAsync(string data)
+        {
+            string url = "http://127.0.0.1:5000/api/values";
+            HttpClient client = new HttpClient();
+            HttpContent content = new StringContent(data);
+            HttpResponseMessage response = await client.PostAsync(url, content);
+            using(HttpContent responseContent = response.Content)
+            {
+                return await responseContent.ReadAsStringAsync();
+            }
+        }
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
