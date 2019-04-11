ad5d77ba5c327853394cfb41e280d9eec3445fed
yahya <yahyaislamov15@mail.ru>
2019-04-11 23:28:25 +0300

    начал 7 лабу

---
+++ lab7/ds-1-master/src/TextRankCalc/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using StackExchange.Redis;
+using System.Text.RegularExpressions;
+using consts;
+
+
+
+namespace TextRankCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var subsc = DB.redis.GetSubscriber();
+            subsc.Subscribe("events", (channel, id) => {
+                string m_id = id;
+                IDatabase db = DB.redis.GetDatabase();
+                Console.WriteLine($"Region: {Consts.GetRegionToView(Consts.GetRegionCode(db.StringGet(m_id)))}");
+                Console.WriteLine($"Id: {id}");
+                db.ListLeftPush( Consts.QUEUE_NAME_COUNTER, m_id, flags: CommandFlags.FireAndForget );
+                db.Multiplexer.GetSubscriber().Publish( Consts.QUEUE_CHANNEL_COUNTER, "" );
+            });
+            Console.ReadKey();
+        }
+    }
+}
